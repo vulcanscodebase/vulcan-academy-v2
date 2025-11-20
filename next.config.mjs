6235d39e26cd2f1be4ced97e18a611729f560eae
vedanthvnayak <vedanthv999@gmail.com>
2025-11-20 22:54:52 +0530

    docker image added

--- next.config.mjs
+++ next.config.mjs
@@ -9,6 +9,7 @@ const nextConfig = {
   images: {
     domains: ['example.com', 'cdn.example.com'], // add the domains used in your course thumbnails
   },
+  output: 'standalone',
 };
 
 export default nextConfig;
